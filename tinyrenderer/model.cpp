6b0c313ab079aa159fcf65810465ff14da877e16
Wei Jie Lim <marvinl@ece019.ece.local.cmu.edu>
2026-03-30 09:15:25 -0400

    init tinyrenderer commit e42447a

---
+++ tinyrenderer/model.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <sstream>
+#include "model.h"
+
+Model::Model(const std::string filename) {
+    std::ifstream in;
+    in.open(filename, std::ifstream::in);
+    if (in.fail()) return;
+    std::string line;
+    while (!in.eof()) {
+        std::getline(in, line);
+        std::istringstream iss(line.c_str());
+        char trash;
+        if (!line.compare(0, 2, "v ")) {
+            iss >> trash;
+            vec3 v;
+            for (int i : {0,1,2}) iss >> v[i];
+            verts.push_back(v);
+        } else if (!line.compare(0, 3, "vn ")) {
+            iss >> trash >> trash;
+            vec3 n;
+            for (int i : {0,1,2}) iss >> n[i];
+            norms.push_back(normalized(n));
+        } else if (!line.compare(0, 2, "f ")) {
+            int f,t,n, cnt = 0;
+            iss >> trash;
+            while (iss >> f >> trash >> t >> trash >> n) {
+                facet_vrt.push_back(--f);
+                facet_nrm.push_back(--n);
+                cnt++;
+            }
+            if (3!=cnt) {
+                std::cerr << "Error: the obj file is supposed to be triangulated" << std::endl;
+                return;
+            }
+        }
+    }
+    std::cerr << "# v# " << nverts() << " f# "  << nfaces() << std::endl;
+}
+
+int Model::nverts() const { return verts.size(); }
+int Model::nfaces() const { return facet_vrt.size()/3; }
+
+vec3 Model::vert(const int i) const {
+    return verts[i];
+}
+
+vec3 Model::vert(const int iface, const int nthvert) const {
+    return verts[facet_vrt[iface*3+nthvert]];
+}
+
+vec3 Model::normal(const int iface, const int nthvert) const {
+    return norms[facet_nrm[iface*3+nthvert]];
+}
+
